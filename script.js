381c229c08c8ca0cb84929e9f9f48bff6fbcddc9
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-06 20:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let submit = document.getElementById("submit");
+submit.addEventListener("click",function(event){
+    event.preventDefault();
+    let fname = document.getElementById("firstname").value;
+    let lname = document.getElementById("LastName").value;
+    let phone = document.getElementById("phnbr").value;
+    let email = document.getElementById("emid").value;
+alert(fname)
+alert(lname)
+alert(phone)
+alert(email)
+})
+
